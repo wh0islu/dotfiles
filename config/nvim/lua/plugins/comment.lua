acc5bc341e088dc29279c50fdb28dbf413d6628a
kaizen <bcc8a62d41d282a9fe0ec7ae4c8b57bb@proton.me>
2026-09-03 12:37:26 -0400

    fix(comment): ajustes

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -1,1 +1,13 @@
-require('Comment').setup()
+require("Comment").setup({
+  mappings = false,
+})
+
+vim.keymap.set("n", "<leader>/", "<Plug>(comment_toggle_linewise_current)", {
+  remap = true,
+  desc = "Alternar comentario da linha",
+})
+
+vim.keymap.set("x", "<leader>/", "<Plug>(comment_toggle_linewise_visual)", {
+  remap = true,
+  desc = "Alternar comentario da selecao",
+})
